fda16cf641082c0578710b9f35f2b06b1ea5a0c9
Julien Rousseau <julien-rousseau@hotmail.fr>
2019-05-09 11:43:25 +0200

    fix show view issues

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,26 +8,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+/**
+ * Class BlogController
+ * @package App\Controller
+ * @Route(name="blog_")
+ */
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="blog_index")
+     * @Route("/blog", name="index")
      */
     public function index()
     {
-        return $this->render('blog/index.html.twig', [
+        return $this->render('index.html.twig', [
             'owner' => 'Thomas',
         ]);
     }
 
-
     /**
-     * @Route("/blog/show/{slug}, name="blog_show" )
+     * @param $slug
+     * @return Response
+     * @Route("/blog/show/{slug}", name="show")
      */
     public function show($slug)
     {
-        return $this->render('blog/show.html.twig', [
+        return $this->render('show.html.twig', [
             'slug' => $slug,
         ]);
     }
