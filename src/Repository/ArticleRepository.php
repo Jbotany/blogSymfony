e5b9f5b2880482e53a904e15de2907480787c22a
Julien Rousseau <julien-rousseau@hotmail.fr>
2019-06-12 10:23:02 +0200

    show tags in article with query builder

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -32,7 +32,10 @@ class ArticleRepository extends ServiceEntityRepository
     public function findAllWithCategoriesAndTags()
     {
         $em = $this->getEntityManager();
-        $query = $em->createQuery('SELECT a, c, t FROM App\Entity\Article a INNER JOIN a.category c INNER JOIN a.tags t');
+        $query = $em->createQuery('SELECT a, c, t 
+        FROM App\Entity\Article a 
+        INNER JOIN a.category c 
+        LEFT JOIN a.tags t');
 
         return $query->execute();
     }
